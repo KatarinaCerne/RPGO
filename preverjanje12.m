e1a647e595c6584d88d4e059f483f4fd86d8d202
Katarina Černe <ktrn.crn@gmail.com>
2018-12-12 09:56:28 +0100

    nal12

--- preverjanje12.m
+++ preverjanje12.m
@@ -4,3 +4,13 @@ P = [P1(:) P2(:) P3(:)];
 m = 5;
 n = 6;
 [Bx,By,Bz] = lsqbezier2(m,n,P,(P(:,1)+3)/6,(P(:,2)+3)/6);
+
+
+% u=linspace(0,1,50);
+% v=linspace(0,1,50);
+% 
+% [bx,by,bz]=bezier2(Bx,By,Bz,u,v);
+% hold on
+% surf(bx,by,bz)
+% mesh(Bx,By,Bz,'Facecolor','none')
+% hold off
